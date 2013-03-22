8ab74336953d23bc4e3c1c61bfb1f7f127d843d2
rolando <rolandosegura@gmail.com>
2013-03-22 13:32:49 -0600

    Add line number in output

--- nutf8/main.go
+++ nutf8/main.go
@@ -14,14 +14,14 @@ func main() {
 	if flag.NArg() < 1 {
 		log.Fatal("need a file name")
 	}
-	f, err := os.Open(flag.Args()[0])
+	fname := flag.Args()[0]
+	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatal("error on open ", err)
 	}
 	defer f.Close()
 	buf := make([]byte, 1024, 1024)
-	var nc int
-	for {
+	for nc, nl := 0, 1; ; {
 		n, err := f.Read(buf)
 		if n == 0 && err == io.EOF {
 			break
@@ -34,13 +34,16 @@ func main() {
 			r, sz := utf8.DecodeRune(b)
 			if r == utf8.RuneError && sz == 1 {
 				fmt.Println()
-				log.Printf("found invalid utf8 rune at byte %v %x", nc, b[0])
+				log.Printf("%v:%v found invalid utf8 rune at byte %v %x", fname, nl, nc, b[0])
 				b = b[1:]
 				continue
 			}
 			fmt.Printf("%c", r)
 			b = b[sz:]
 			nc += sz
+			if r == '\n' {
+				nl++
+			}
 		}
 	}
 }
